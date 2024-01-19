a5781794500db9b09a832613b305b703fb095cf7
sergio sanabria <sergio_200404@hotmail.com>
2024-01-19 12:08:50 -0300

    resolucion de ejercicio parte 2

--- M05 Fundamentos Javascript/ejercicios/33.js
+++ M05 Fundamentos Javascript/ejercicios/33.js
@@ -6,6 +6,12 @@ function esNumeroPrimo(numero) {
   // IMPORTANTE: Recuerda que un número primo es aquel que
   // solo es divisible por sí mismo y por 1.
   // Tu código:
+  if (numero / 1 === numero && numero / numero === 1){
+    return true
+  } else {
+    return false
+  }
+
 }
 
 module.exports = esNumeroPrimo;
