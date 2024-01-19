f4c83f43393370c091f05155ccfc0e8aeda94eba
SergioMSanabria <sergio_200404@hotmail.com>
2024-01-18 22:50:44 -0300

    ejercicios del 1 al 8

---
+++ M05 Fundamentos Javascript/Demo/funciones.js
@@ -0,0 +1,24 @@
+function saludarmañana(){
+    console.log("hola mundo")
+    return 1
+}
+
+function despedirsenoche(){
+    console.log("hasta mañana mundo")
+
+}
+
+saludarmañana()
+despedirsenoche()
+
+if(10 > 5){
+    console.log("10 es mayor que 5")
+} else {
+    console.log("10 es menor que 5")
+}
+
+var resultados = Math.ceil(3.26)
+var resultados2 = Math.floor(3.26)
+
+console.log(resultados)
+console.log(resultados2)
---
+++ M05 Fundamentos Javascript/Demo/variables.js
@@ -0,0 +1,23 @@
+var nombre1 = "sergio"
+var nombre2 = "1992"
+
+var numero = 1992
+var decimal = 19.92
+var negativo = -1992
+
+var booleano = true
+var booleano2 = false
+
+var nulo = null
+
+var indefinido = undefined
+
+console.log(typeof nombre1)
+console.log(typeof decimal)
+
+console.log(nombre1)
+console.log(nombre2)
+console.log(numero,booleano,nulo,booleano)
+console.log(20%5)
+console.log(20/5)
+console.log("operacion "+"de "+ "concatenacion "+"de string")
