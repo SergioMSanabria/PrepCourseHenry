d0bd8db6245aa92256337048c0000539b222d93f
sergio sanabria <sergio_200404@hotmail.com>
2024-01-20 12:56:41 -0300

    ejercicios de bucles del 1 al 6

--- M06 Bucles/ejercicios/06.js
+++ M06 Bucles/ejercicios/06.js
@@ -2,6 +2,15 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  if (num > 99 && num < 1000){
+    return true
+  }
+  if (num > -1000 || num < -99){
+    return false
+  }
+  if (num > 99 || num < 1000){
+    return false
+  } 
 }
 
 module.exports = tieneTresDigitos;
